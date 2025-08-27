b1b60381300c5689de8052eb66f2072f7facded6
CristianCallahuanca <ccallahuanca@frba.utn.edu.ar>
2025-08-27 13:40:58 -0300

    Hasta aca solo se movio lo de estatica y se crearon clases para dinamica (SIN LOGICA)

--- loader-dinamico/src/main/java/org/example/metamapa/dinamico/models/entidades/hecho/HechoCrudo.java
+++ loader-dinamico/src/main/java/org/example/metamapa/dinamico/models/entidades/hecho/HechoCrudo.java
@@ -4,3 +4,6 @@ public class HechoCrudo {
 }
 // quizas tengamos que tener en cuenta que para respaldar este hecho en la BD de la dinamica tiene que tener un ID
 // En caso de que se quiera modificar segun el contribuyente
+
+
+//
