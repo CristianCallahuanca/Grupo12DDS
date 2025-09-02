bf3dee3fd8b38e6fb34c4b291ef6916552281e99
joaco23 <jobarreiro@frba.utn.edu.ar>
2025-09-02 11:27:03 -0300

    se pueden subir usuarios, hechos y obtener todos

---
+++ loader-dinamico/src/main/java/dinamico/models/dtos/input/usuarioDTO.java
@@ -0,0 +1,13 @@
+package dinamico.models.dtos.input;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class usuarioDTO {
+    String nombre;
+    String apellido;
+    String edad;
+    String dni;
+}
