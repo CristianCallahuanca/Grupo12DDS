0b3d6719ec3ded46d8e86ae09da15679a35fddfa
CristianCallahuanca <ccallahuanca@frba.utn.edu.ar>
2025-09-19 13:17:27 -0300

    division modulo loader proxy

---
+++ loader-demo/src/main/java/org/example/metamapa/loaderdemo/LoaderDemoApplication.java
@@ -0,0 +1,11 @@
+package org.example.metamapa.loaderdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoaderDemoApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(LoaderDemoApplication.class, args);
+    }
+}
