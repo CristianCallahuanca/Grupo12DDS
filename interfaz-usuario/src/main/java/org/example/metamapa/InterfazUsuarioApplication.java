641303f77e873c6adf4c546b0104cac72b19a8e8
CristianCallahuanca <ccallahuanca@frba.utn.edu.ar>
2025-10-13 18:38:26 -0300

    interfaz de usuario modulo

---
+++ interfaz-usuario/src/main/java/org/example/metamapa/InterfazUsuarioApplication.java
@@ -0,0 +1,11 @@
+package org.example.metamapa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterfazUsuarioApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(InterfazUsuarioApplication.class, args);
+    }
+}
