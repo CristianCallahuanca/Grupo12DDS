95be55ba9b20bc581cb4a29ffb84bf0d59a03d62
CristianCallahuanca <ccallahuanca@frba.utn.edu.ar>
2025-10-17 10:50:10 -0300

    Limpio hardcodeo, deberia funcionar igual

--- gestor-datos/src/main/java/org/example/metamapa/gestordatos/CorsConfig.java
+++ gestor-datos/src/main/java/org/example/metamapa/gestordatos/CorsConfig.java
@@ -1,25 +1,35 @@
 package org.example.metamapa.gestordatos;
 
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import java.util.List;
+
 @Configuration
+@ConfigurationProperties(prefix = "app.cors")
 public class CorsConfig {
 
+    private List<String> allowedOrigins = List.of();
+
+    //TODO: JOACO, esto probalo, la idea es no hardcodear y aca te deje una forma para toda configuracion centrarlo en el .yml
     @Bean
     public WebMvcConfigurer corsConfigurer() {
         return new WebMvcConfigurer() {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:8090", "http://127.0.0.1:5500", "http://localhost:3000")
+                        .allowedOrigins(allowedOrigins.toArray(String[]::new))
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true);
             }
         };
     }
-
+    public void setAllowedOrigins(List<String> allowedOrigins) {
+        this.allowedOrigins = allowedOrigins;
+    }
 }
+
