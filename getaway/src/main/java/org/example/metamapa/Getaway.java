8a07e17bf82e7dc0d2eb54940d486e6259a21643
joaquinbarreiro23 <joaquin.s.barreiro@gmail.com>
2025-12-13 16:35:11 -0300

    arreglo del getaway soporta posts

--- getaway/src/main/java/org/example/metamapa/Getaway.java
+++ getaway/src/main/java/org/example/metamapa/Getaway.java
@@ -6,6 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Getaway {
     public static void main(String[] args) {
+
         SpringApplication.run(Getaway.class, args);
     }
 }
