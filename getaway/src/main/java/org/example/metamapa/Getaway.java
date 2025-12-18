b98cd300521fc6ac7212a34d61c3790d49d4d7d2
CristianCallahuanca <ccallahuanca@frba.utn.edu.ar>
2025-12-18 10:08:27 -0300

    fix gateway

--- getaway/src/main/java/org/example/metamapa/Getaway.java
+++ getaway/src/main/java/org/example/metamapa/Getaway.java
@@ -1,9 +1,12 @@
 package org.example.metamapa;
 
+import org.example.metamapa.Config.GatewayProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
+@EnableConfigurationProperties(GatewayProperties.class)
 public class Getaway {
     public static void main(String[] args) {
 
